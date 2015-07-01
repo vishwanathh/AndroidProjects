f35f791a721c81926e6b32abbb25ba2538d0fcc7
Vishwanath Hariharan <vishwanath.hariharan@ericsson.com>
2015-07-01 23:45:52 +0530

    A simple images grid view project

---
+++ android-simple-gridview/src/com/example/hellogridview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.hellogridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+	    gridview.setAdapter(new ImageAdapter(this));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v,
+	                int position, long id) {
+	            Toast.makeText(MainActivity.this, "" + position,
+	                    Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    
+	    gridview.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Toast.makeText(MainActivity.this, "Long Pressed : " + position,
+	                    Toast.LENGTH_SHORT).show();
+				return true;
+			}
+	    });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {		
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
